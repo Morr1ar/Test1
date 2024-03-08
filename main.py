1f27cd8d0fd8ca53d055eb7a2745d9f9306159de
Morr1ar <minenkov06@mail.ru>
2024-03-08 13:34:12 +0300

    Задача 1.1

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('test')
+import csv
+
+with open('students.csv', encoding='utf-8') as file:
+    reader = csv.reader(file, delimiter=',')
+    for s in reader:
+        if 'Хадаров Владимир' in s[1]:
+            print(f'Ты получил: {s[-1]} за проект - {s[2]}')
+            break
