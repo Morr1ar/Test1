3e6f1b05e9f1e824fc63ec7a994cab25d647bc89
Morr1ar <minenkov06@mail.ru>
2024-03-09 10:27:16 +0300

    Задача 3

---
+++ task3.py
@@ -0,0 +1,18 @@
+import csv
+
+with open('students.csv', encoding='utf-8') as file:
+    reader = list(csv.reader(file, delimiter=','))
+header = reader[0]
+reader = reader[1:]
+
+n = input()
+while n != 'СТОП':
+    flag = 1
+    for s in reader:
+        if s[0] == n:
+            print(f'Проект № {s[2]} делал: {s[1]} он(а) получил(а) оценку - {s[-1]}.')
+            flag = 0
+            break
+    if flag:
+        print('Ничего не найдено.')
+    n = input()
